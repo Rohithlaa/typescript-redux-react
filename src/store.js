fc44856da40ae4e5e99869053fcc850ad63a7891
indianic <indianic@indianics-iMac-Pro.local>
2021-10-13 17:01:31 +0530

    applying middle ware

--- src/store.js
+++ src/store.js
@@ -1,9 +1,9 @@
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import rootReducer from "./Reducers/rootReducer";
 
-const store = createStore(rootReducer, composeWithDevTools((thunk)))
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
 
 
 export default store
